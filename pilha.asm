613513a1f94a66d91b33c3c0f172234aafd006c7
Renato Soares <renatosoarespro@hotmail.com>
2015-06-02 17:16:10 -0300

    :checkered_flag:pilha push, pop e size

--- pilha.asm
+++ pilha.asm
@@ -1,7 +1,6 @@
 # pilha com push, pop e o tamanho.
 .data 
 endPilha:	.word 	0:10	# Espaço reservado na memória para armazenar endereço
-pilha:		.word 	0:10	# Espaço reservado na memória para armazenar dados
 size:		.word 	0	# quantidade de elemento do array
 espaco:		.asciiz	" "
 quebra:		.asciiz	"\n"
@@ -13,12 +12,16 @@ main:
 #--------------------------------------------------------------------------------------------
 
 # carregue endereço da memória para armazenar endereço da memória de dados
-#lui	$at, 0x1001
-#add	$s0, $zero, $at
+lui	$at, 0x1001
+add	$s0, $zero, $at
+
+
 
 # carregue endereço da memória de dados
 lui	$at, 0x1001
 addi	$s1, $at, 0x4C
+
+add	$t7, $t7, $s0		# ponteiro para o inicio da pilha
 
 
 # carrega quantidade de elementos do array
@@ -49,20 +52,9 @@ lw	$s5, 0($s5)
 	
 
 #--------------------------------------------------------------------------------------------
-###################	Nós para pilha			####################################|
+###################				####################################|
 #--------------------------------------------------------------------------------------------	
-	recordNode:
 
-
-		#jr	$ra 		# volta para função push no edereço marcado pelo jal
-				
-	j functionSelecao
-	
-	recordStack:
-		#lui	$at, 0x1001
-		#add	$s0, $zero, $at
-		#jr	$ra
-	j functionSelecao
 #============================================================================================
 
 #--------------------------------------------------------------------------------------------
@@ -70,37 +62,17 @@ lw	$s5, 0($s5)
 #--------------------------------------------------------------------------------------------	
 	
 	functionPush:
-		
-		
 		addi	$v0, $zero, 5
 		syscall
+		
 		add	$s2, $zero, $v0
 		
 		addi 	$t9, $t9, 1	#contador de elementos
 		
-		add	$t7, $zero, $s1
-		lui	$at, 0x1001
-		add	$s0, $zero, $at
-		add	$t8, $zero, $t9
-		
-		loopArmazenaEnd:
-			
-			sw	$t7, 0($s0)	# armazena endereço
-			addi	$s0, $s0, 4
-			addi	$t7, $t7, 4
-			
-			addi	$t8, $t8, -1
-			bgtz	$t8, loopArmazenaEnd
-			
-		sw	$s2, 0($s1) 	# armazena dados		
-						
-										
-		#jal	recordNode
-		
+		sw	$s2, 0($s0)	# armazena dados
+		addi	$s0, $s0, 4
+		addi	$t7, $t7, 4	# aponta para cabeça da pilha
 
-		#addi	$s0, $s0, 4
-		addi	$s1, $s1, -4		
-	
 	j	functionSelecao
 #============================================================================================
 
@@ -108,7 +80,19 @@ lw	$s5, 0($s5)
 ###################	função pop()			####################################|
 #--------------------------------------------------------------------------------------------	
 	functionPop:
-					
+	
+		lw 	$t1, -4($t7)
+		# imprima o topo da pilha
+		add	$a0, $zero, $t1 
+		addi	$v0, $zero, 1
+		syscall				
+		
+		sw	$zero, -4($t7)
+		
+		addi	$s0, $s0, -4
+		addi	$t7, $t7, -4
+		addi 	$t9, $t9, -1
+		
 	j	functionSelecao
 #============================================================================================
 
@@ -116,7 +100,10 @@ lw	$s5, 0($s5)
 ###################	função size()			####################################|
 #--------------------------------------------------------------------------------------------	
 	functionSize:
-		
+	# imprima o topo da pilha
+		add	$a0, $zero, $t9 
+		addi	$v0, $zero, 1
+		syscall						
 	j	functionSelecao
 #============================================================================================
 
