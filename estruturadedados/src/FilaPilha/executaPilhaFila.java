551fd8d2d7c9edb8bda6046a36f701fe7af72dd1
Renato Soares <renatosoarespro@hotmail.com>
2015-09-05 17:45:42 -0300

    reformulando fila

---
+++ estruturadedados/src/FilaPilha/executaPilhaFila.java
@@ -0,0 +1,17 @@
+package FilaPilha;
+
+public class executaPilhaFila {
+
+	public executaPilhaFila() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
